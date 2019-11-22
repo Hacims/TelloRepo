a7bc1762b06a8e348a4a87902ae3e2155be57bcb
rsknox <rsknox@users.noreply.github.com>
2019-11-22 12:06:39 -0600

    to __init__ added commands with 5 sec delays between to try to get better images

--- 00EasyTello/__init__.py
+++ 00EasyTello/__init__.py
@@ -18,10 +18,24 @@ my_drone.streamon()
 
 
 my_drone.takeoff()
-
-for i in range(4):
-    my_drone.forward(50)
-    my_drone.cw(90)
+time.sleep (5)
+my_drone.up(20)
+time.sleep(5)
+my_drone.set_speed(10)
+time.sleep(5)
+my_drone.forward(200)
+time.sleep(5)
+my_drone.cw(90)
+time.sleep(5)
+my_drone.forward(20)
+time.sleep(5)
+my_drone.cw(90)
+time.sleep(5)
+my_drone.forward(200)
+time.sleep(5)
+# for i in range(4):
+#     my_drone.forward(50)
+#     my_drone.cw(90)
 
 my_drone.land()
 
